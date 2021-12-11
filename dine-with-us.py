d487c9b56674831b802a990d63e01c2bc1fe7bc5
cw118 <carolyn94118@gmail.com>
2021-12-11 11:52:51 -0500

    Initialize db tables, modify ui

--- dine-with-us.py
+++ dine-with-us.py
@@ -5,6 +5,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -27,6 +28,27 @@ Session(app)
 # Database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# User database table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+# Posts database table
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
 
 @app.route('/')
 @app.route('/index')
